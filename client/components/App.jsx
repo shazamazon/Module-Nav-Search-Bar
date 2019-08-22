511a277846fdb3ad9d2d240c23f87d6eb85f3643
Sean McCarthy <seanmccarthy3223@GMAIL.com>
2019-08-21 19:54:59 -0500

    set up webpack and link starter files to local server on port 3003

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <h1>Great Job from React!!!</h1>
+    );
+  }
+}
+
+export default App;
