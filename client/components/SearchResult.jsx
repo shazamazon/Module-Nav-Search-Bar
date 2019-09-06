a6e6831947b96c61990329116644dd80fc38fa8a
Sean McCarthy <seanmccarthy3223@GMAIL.com>
2019-09-06 09:50:12 -0500

    Add window event to send product id to all services

--- client/components/SearchResult.jsx
+++ client/components/SearchResult.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const SearchResult = (props) => {
+  const addEventListener = (id) => () => {
+    const event = new CustomEvent('clickedProduct', {detail: id});
+    window.dispatchEvent(event);
+  };
 
+  return (
+    <li className='nav-search-result' onClick={addEventListener(props.result.ProductId)}>{props.result.ItemName}</li>
+  );
 };
 
 export default SearchResult;
